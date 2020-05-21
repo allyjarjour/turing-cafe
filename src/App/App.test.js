e51a0c03e4608e2c7f8d744d2f9ac4bfb8b2a070
Ally Jarjour <allyjarjour@gmail.com>
2020-05-21 11:50:31 -0600

    Add Integration test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from "./App";
+import React from "react";
+import { render, rerender, waitFor, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Turing Cafe Reservations")).toBeInTheDocument();
+  });
+  it('a new card should be added when user clicks make reservation button', async () => {
+    const { getByPlaceholderText, getByText, getAllByTestId, debug } = render(<App />);
+    fireEvent.change(getByPlaceholderText("Name"), {target: { value: "Sam" }});
+    fireEvent.change(getByPlaceholderText("Date"), {target: { value: "2/10" }});
+    fireEvent.change(getByPlaceholderText("Time"), {target: { value: "1:30" }});
+    fireEvent.change(getByPlaceholderText("Number of guests"), {target: { value: "5" }});
+    fireEvent.click(getByText("Make Reservation"));
+    await waitFor(() => {
+      expect(getByText('Sam')).toBeInTheDocument()
+      expect(getByText('2/10')).toBeInTheDocument()
+      expect(getByText('1:30')).toBeInTheDocument()
+      expect(getByText('5')).toBeInTheDocument()
+      // expect(getAllByTestId('reservation-card')).toHaveLength(10)
+    })
+  })
+
+})
--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -6,7 +6,7 @@ export default class ReservationCard extends Component {
   render() {
     const { name, date, time, number } = this.props
     return (
-      <div className="reservation-card">
+      <div className="reservation-card" data-testid="reservation-card" >
         <p>{name}</p>
         <p>{date}</p>
         <p>{time}</p>
